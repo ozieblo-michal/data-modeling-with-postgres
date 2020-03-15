2bb3ed034b3a1fe0d23d101655bd4a5d99f24d8d
ozieblo-repository <59211762+ozieblo-repository@users.noreply.github.com>
2020-03-15 19:00:19 +0100

    resubmission
    2 REQUIRED CHANGES:
    - the Table TIME currently does not specify which primary key should exist. In addition, foreign keys in Table SONGPLAYS do not have NOT NULLs indicated
    - table TIME requires DO NOTHING statement similar to Table ARTISTS that you have done

--- sql_queries.py
+++ sql_queries.py
@@ -9,10 +9,10 @@ time_table_drop = "DROP TABLE IF EXISTS time;"
 # CREATE TABLES
 
 songplay_table_create = "CREATE TABLE IF NOT EXISTS songplays (songplay_id SERIAL PRIMARY KEY, start_time TIMESTAMP, user_id INT, level VARCHAR, song_id VARCHAR, artist_id VARCHAR, session_id VARCHAR, location VARCHAR, user_agent VARCHAR);"
-user_table_create = "CREATE TABLE IF NOT EXISTS users (user_id VARCHAR PRIMARY KEY, first_name VARCHAR, last_name VARCHAR, gender VARCHAR, level VARCHAR);"
-song_table_create = "CREATE TABLE IF NOT EXISTS songs (song_id VARCHAR PRIMARY KEY, title VARCHAR, artist_id VARCHAR NOT NULL, year VARCHAR, duration NUMERIC);"
-artist_table_create = "CREATE TABLE IF NOT EXISTS artists (artist_id VARCHAR PRIMARY KEY, name VARCHAR, location VARCHAR, latitude VARCHAR, longitude VARCHAR);"
-time_table_create = "CREATE TABLE IF NOT EXISTS time (start_time TIMESTAMP, hour INT, day INT, week INT, month INT, year INT, weekday TEXT);"
+user_table_create = "CREATE TABLE IF NOT EXISTS users (user_id VARCHAR PRIMARY KEY NOT NULL, first_name VARCHAR, last_name VARCHAR, gender VARCHAR, level VARCHAR);"
+song_table_create = "CREATE TABLE IF NOT EXISTS songs (song_id VARCHAR PRIMARY KEY NOT NULL, title VARCHAR, artist_id VARCHAR NOT NULL, year VARCHAR, duration NUMERIC);"
+artist_table_create = "CREATE TABLE IF NOT EXISTS artists (artist_id VARCHAR PRIMARY KEY NOT NULL, name VARCHAR, location VARCHAR, latitude VARCHAR, longitude VARCHAR);"
+time_table_create = "CREATE TABLE IF NOT EXISTS time (start_time TIMESTAMP PRIMARY KEY NOT NULL, hour INT, day INT, week INT, month INT, year INT, weekday TEXT);"
 
 # INSERT RECORDS
 
@@ -20,7 +20,7 @@ songplay_table_insert = "INSERT INTO songplays (start_time, user_id, level, song
 user_table_insert = "INSERT INTO users (user_id, first_name, last_name, gender, level) VALUES (%s, %s, %s, %s, %s) ON CONFLICT (user_id) DO UPDATE SET level = EXCLUDED.level;"
 song_table_insert = "INSERT INTO songs (song_id, title, artist_id, year, duration) VALUES (%s, %s, %s, %s, %s) ON CONFLICT (song_id) DO NOTHING;"
 artist_table_insert = "INSERT INTO artists (artist_id, name, location, latitude, longitude) VALUES (%s, %s, %s, %s, %s) ON CONFLICT (artist_id) DO NOTHING;"
-time_table_insert = "INSERT INTO time (start_time, hour, day, week, month, year, weekday) VALUES (%s, %s, %s, %s, %s, %s, %s);"
+time_table_insert = "INSERT INTO time (start_time, hour, day, week, month, year, weekday) VALUES (%s, %s, %s, %s, %s, %s, %s) ON CONFLICT (start_time) DO NOTHING;"
 
 # FIND SONGS
 
